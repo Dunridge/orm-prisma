f1f74ff16b0eb07549299ab2e40dd1df40da3b5f
Dunridge <Polinka7max@gmail.com>
2022-02-19 23:21:36 +0200

    develop. added ORM abstraction imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import { PrismaClient } from '@prisma/client'
+// TODO: resolve the PrismaClient import error
 const prisma = new PrismaClient()
 
 console.log(prisma.human);
+
+// TODO: connect the TypeScript here
+
+const humansPromise = prisma.human.findMany({
+    where: {
+        name: {
+            contains: 'mom'
+        }
+    }
+});
+
+humansPromise.then(data => {
+    console.log(data);
+})
